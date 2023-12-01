84fcc0c77c31ed05a4b5c88f94014be88b1767c1
UserNameMVS <vlad.muravyov@mail.ru>
2023-12-02 04:38:48 +0500

    initial project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { App } from './App'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import ThemeProvider from './theme/ThemeProvider'
+
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+)
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,28 @@
+import { FC, useMemo, useState } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.DARK
+
+const ThemeProvider: FC<Props> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === Theme.DARK ? Theme.LIGHT : Theme.DARK))
+  }
+
+  const defaultProps = useMemo(
+    () => ({
+      theme,
+      setTheme: toggleTheme,
+    }),
+    [theme]
+  )
+
+  return <ThemeContext.Provider value={defaultProps}>{children}</ThemeContext.Provider>
+}
+
+export default ThemeProvider
